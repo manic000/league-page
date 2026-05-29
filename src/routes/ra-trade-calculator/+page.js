01df2bf94d56f4d49bf692238dbf1763c2ab47a7
manic000 <manic000@gmail.com>
2026-05-29 16:32:06 -0500

    ..

--- src/routes/ra-trade-calculator/+page.js
+++ src/routes/ra-trade-calculator/+page.js
@@ -1,11 +1,11 @@
-import { getLeagueRosters, getLeagueTeamManagers, getPlayers } from '$lib/utils/helper';
+import { getLeagueRosters, getLeagueTeamManagers, loadPlayers } from '$lib/utils/helper';
 
 export async function load() {
     // Fetch all required league data concurrently
     const [rostersData, managersData, playersData] = await Promise.all([
         getLeagueRosters(),
         getLeagueTeamManagers(),
-        getPlayers()
+        loadPlayers()
     ]);
     
     return {
