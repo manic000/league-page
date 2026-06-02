2fe7724ff0fafbda0484c96f1cb7c30c5e8dc00e
manic000 <manic000@gmail.com>
2026-06-02 14:16:00 -0500

    ..

--- src/routes/ra-trade-calculator/+page.js
+++
@@ -1,8 +0,0 @@
-import { getRaPickValues } from '$lib/utils/helperFunctions/raPickValues';
-
-export async function load({ fetch }) {
-    const valueData = getRaPickValues(fetch);
-    return {
-        valueData
-    };
-}
--- src/routes/ra-trade-calculator/+page.svelte
+++
@@ -1,7 +0,0 @@
-<script>
-    import { RATradeCalculator } from '$lib/components';
-
-    let { data } = $props();
-</script>
-
-<RATradeCalculator valueData={data.valueData} />
